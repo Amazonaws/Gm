3ab8ef4beee1cf03a2294e574f77dd703e9bf36c
Amazonaws <alex.audibert837@orange.fr>
2014-10-03 10:07:55 +0200

    Create error.xml.php

---
+++ error.xml.php
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="<?php echo sfConfig::get('sf_charset', 'UTF-8') ?>"?>
+<error>
+   <code><?php echo $code ?></code>
+   <message><?php echo $text ?></message>
+</error>
